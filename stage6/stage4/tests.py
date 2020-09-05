3c28e9d2cafd5813501fb0f4280554acf7b8063c
YopsiQ <yopsiq@gmail.com>
2020-09-05 21:38:55 +0300

    Fixed directory.

---
+++ stage6/stage4/tests.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from hstest.test_case import TestCase
+
+from base import HyperTubeTest
+
+
+class HyperTubeTestRunner(HyperTubeTest):
+    def generate(self):
+        return [
+            # 1 task
+            TestCase(attach=self.check_server),
+            TestCase(attach=self.check_create_videos),
+            # 2 task
+            TestCase(attach=self.check_main_header),
+            TestCase(attach=self.check_main_page_login_link),
+            TestCase(attach=self.check_main_page_upload_link),
+            TestCase(attach=self.check_main_page_video_links),
+            TestCase(attach=self.check_main_page_video_count),
+            # 3 task
+            TestCase(attach=self.check_main_page_search),
+            TestCase(attach=self.check_main_page_tag_filtering),
+            # 4 task
+            TestCase(attach=self.check_signup),
+            TestCase(attach=self.check_login),
+        ]
+
+    def check(self, reply, attach):
+        return attach()
+
+
+if __name__ == '__main__':
+    HyperTubeTestRunner('hypertube.manage').run_tests()
