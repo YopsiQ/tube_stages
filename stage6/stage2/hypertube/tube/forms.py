0ce50d62de844c36409f17baad4693c8656202d1
YopsiQ <yopsiq@gmail.com>
2020-09-07 21:50:35 +0300

    Refactoring: removed redundant folders and files.

--- stage6/stage2/hypertube/tube/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class UploadFileForm(forms.Form):
-    video = forms.FileField(label='Select a video')
-    title = forms.CharField(label='Title', max_length=255)
-    tags = forms.CharField(label='Point tags', max_length=255)
